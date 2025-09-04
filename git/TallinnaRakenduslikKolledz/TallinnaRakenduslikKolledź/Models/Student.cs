fce7c6945d3fe46329ed4847d1873fc765a6d1c1
Opilane TTHK <opilane@tthk.ee>
2025-09-04 12:58:46 +0300

    Adding the files

---
+++ git/TallinnaRakenduslikKolledz/TallinnaRakenduslikKolledź/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallinnaRakenduslikKolledź.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment>? Enrollments { get; set; }
+
+        /*Lis kolm omadust õpilasele, ise mõtled välja*/
+
+        public string? Gender {  get; set; }
+        public int? Age { get; set; }
+        public string? LanguageGroup { get; set; }
+    }
+}
