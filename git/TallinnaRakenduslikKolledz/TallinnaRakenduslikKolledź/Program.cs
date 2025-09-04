c179abd2ecf39e2b47f65a3e7830af654cba0e3b
ArturVartsaba <arturvartsaba1@gmail.com>
2025-09-04 15:02:38 +0300

    Update

--- git/TallinnaRakenduslikKolledz/TallinnaRakenduslikKolledź/Program.cs
+++ git/TallinnaRakenduslikKolledz/TallinnaRakenduslikKolledź/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Data;
+
 namespace TallinnaRakenduslikKolledź
 {
     public class Program
@@ -8,6 +11,9 @@ namespace TallinnaRakenduslikKolledź
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<SchoolContext>(options => options.UseSqlServer
+                (builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             var app = builder.Build();
 
