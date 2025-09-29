b54d7be4908e9d452cd6b220bf4e08e814913477
ArturVartsaba <ArturVartsaba1@gmail.com>
2025-09-29 14:13:56 +0300

    Added methods to CoursesController

--- git/TallinnaRakenduslikKolledz/TallinnaRakenduslikKolledź/Controllers/CoursesController.cs
+++ git/TallinnaRakenduslikKolledz/TallinnaRakenduslikKolledź/Controllers/CoursesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikKolledz.Data;
+using TallinnaRakenduslikKolledź.Models;
 
 namespace TallinnaRakenduslikKolledz.Controllers
 {
@@ -12,6 +13,7 @@ namespace TallinnaRakenduslikKolledz.Controllers
         {
             _context = context;
         }
+
         public IActionResult Index()
         {
             var courses = _context.Courses.Include(c => c.Department)
@@ -19,6 +21,7 @@ namespace TallinnaRakenduslikKolledz.Controllers
 
             return View(courses);
         }
+
         [HttpGet]
         public IActionResult Create()
         {
@@ -26,11 +29,58 @@ namespace TallinnaRakenduslikKolledz.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Course course) 
+        { 
+            if (!ModelState.IsValid) 
+            {
+                _context.Courses.Add(course);
+                await _context.SaveChangesAsync();
+                PopulateDepartmentsDropDownList(course.DepartmentID);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Courses == null) 
+            { 
+                return NotFound();
+            }
+            var courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CourseId == id);
+            if (courses == null) 
+            { 
+                return NotFound();
+            }
+            return View(courses);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Courses == null) 
+            { 
+                return NotFound();
+            }
+            var course = await _context.Courses.FindAsync(id);
+            if (course != null)
+            {
+                _context.Courses.Remove(course);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
         private void PopulateDepartmentsDropDownList(object selectedDepartment = null)
         {
             var departmentsQuery = from d in _context.Departments
-                orderby d.Name
-                select d;
+                                   orderby d.Name
+                                   select d;
             ViewBag.DepartmentID = new SelectList(departmentsQuery.AsNoTracking(), "DepartmentID", "Name", selectedDepartment);
         }
     }
